d9c8fb4e0eaffd5db023f427681a76190d209b38
Het Modi <het.modi@armakuni.com>
2026-02-23 17:33:45 +0530

    AT3-T2455 practical example of setting the default data in fastapi

--- fast_api/fastapi_bodyparameter.py
+++ fast_api/fastapi_bodyparameter.py
@@ -1,9 +1,12 @@
-from typing import Annotated
-
-from fastapi import FastAPI, Path
-from pydantic import BaseModel
+from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
 
 app = FastAPI()
+
+
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
 
 
 class Item(BaseModel):
@@ -11,20 +14,11 @@ class Item(BaseModel):
     description: str | None = None
     price: float
     tax: float | None = None
+    tags: set[str] = set()
+    image: Image | None = None
 
 
 @app.put("/items/{item_id}")
-async def update_item(
-    item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
-    q: str | None = None,
-    item: Item | None = None,
-):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
-    if item:
-        results.update({"item": item})
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
     return results
-
-
-
